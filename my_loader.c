8c18b9ab6d010fefc45a9f0e0a2e5fadc1024d95
ronshav <ron.sh.10.01@gmail.com>
2025-06-14 20:25:34 +0300

    1b (added peints of flags to readelf_l)

--- my_loader.c
+++ my_loader.c
@@ -5,6 +5,7 @@
 #include <elf.h>
 #include <sys/mman.h>
 
+
 char *format = "%-7s %#08x %#010x %#010x %#09x %#07x %1c%1c%1c %#06x\n";
 
 int checkMagicNumber(unsigned char number[])
@@ -73,7 +74,32 @@ void readelf_L(Elf32_Phdr *phdr, int j)
         flag[2] = 'E';
     }
 
-    printf(format, type, phdr->p_offset, phdr->p_vaddr,  phdr->p_paddr, phdr->p_filesz, phdr->p_memsz, flag[0], flag[1], flag[2], phdr->p_align);
+    printf(format, type, phdr->p_offset, phdr->p_vaddr, phdr->p_paddr, phdr->p_filesz, phdr->p_memsz, flag[0], flag[1], flag[2], phdr->p_align);
+    if (phdr->p_type != PT_DYNAMIC && phdr->p_type != PT_INTERP)
+    {
+        printf("protection flags: ");
+        if (flag[0] == 'R')
+        {
+            printf("PROT_READ ");
+            if (flag[1] == 'W' || flag[2] == 'E')
+                printf("| ");
+        }
+        if (flag[1] == 'W')
+        {
+            printf("PROT_WRITE ");
+            if (flag[2] == 'E')
+                
+            printf("| ");
+        }
+        if (flag[2] == 'E')
+            printf("PROT_EXEC");
+        if(flag[1]==' '&& flag[2]==' '&& flag[3]==' ')
+        {
+            printf("PROT_NONE");
+        }
+        printf("\n");
+        printf("mapping flags: MAP_PRIVATE | MAP_FIXED\n");
+    }
 }
 
 int foreach_phdr(void *map_start, void (*func)(Elf32_Phdr *, int), int arg)
@@ -113,7 +139,7 @@ int main(int argc, char *argv[])
     lseek(op, 0, SEEK_SET);
     int size = lseek(op, 0, SEEK_END);
     lseek(op, 0, SEEK_SET);
-    header = malloc(size);
+    header = malloc(52);
     read(op, header, sizeof(*header));
     if (checkMagicNumber(header->e_ident) == 0) // file is not ELF32
     {
