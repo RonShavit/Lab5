8d99a7895401668d55d22668f56299fdd47f71fd
ronshav <ron.sh.10.01@gmail.com>
2025-06-14 15:24:41 +0300

    added files

--- my_loader.c
+++ my_loader.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <elf.h>
 
 
 
-int main(int argv, char* argc[])
+int main(int argc, char* argv[])
 {
     int op = -1;
-    if (argv<2)
+    Elf32_Ehdr* header = malloc(52);
+    if (argc<2)
     {
         printf("Usage : ./my_loader <file_name>\n");
         exit(0);
     }
-    op = open(argc[1],O_RDWR);
+    op = open(argv[1],O_RDWR);
     if(op<0)
     {
-        fprintf(stderr,"ERROR opening file %s",argc[1]);
+        fprintf(stderr,"ERROR opening file %s",argv[1]);
         perror(" ");
+        exit(0);
     }
+    read(op,header,sizeof(*header));
+    printf("%d\n",header->e_ehsize);
+
+    close(op);
+    free(header);
+
+    
     
 }
