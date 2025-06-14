51e92e4b75d78228ca4bfd27da76cc7f6e8547a8
ronshav <ron.sh.10.01@gmail.com>
2025-06-14 18:34:27 +0300

    up to 1a

--- makefile
+++ makefile
@@ -1,5 +1,10 @@
 my_loader: my_loader.c
-	gcc -g -Wall -m32 -o my_loader my_loader.c
+	gcc -m32 -c my_loader.c -o my_loader.o
+	nasm -f elf32 start.s -o start.o
+	nasm -f elf32 startup.s -o startup.o
+	ld -o my_loader my_loader.o startup.o start.o -L/usr/lib32 -lc -T linking_script -dynamic-linker /lib32/ld-linux.so.2
+
+
 
 clean:
 	rm my_loader
--- my_loader.c
+++ my_loader.c
@@ -3,33 +3,119 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
+#include <sys/mman.h>
+
+char *format = "%-7s %#08x %#010x %#010x %#09x %#07x %1c%1c%1c %#06x\n";
 
 int checkMagicNumber(unsigned char number[])
 {
-    if (number[0] != 0x7f || number[1] != 'E' || number[2] != 'L' || number[3] != 'F' || number[4] != 1)
+    if (number[0] != 0x7f || number[1] != 'E' || number[2] != 'L' || number[3] != 'F' || number[4] != 1) // check if file is ELF32 file
         return 0;
     return 1;
+}
+
+void test_print(Elf32_Phdr *phdr, int i)
+{
+    printf("prog header %d at add %X", i, phdr->p_vaddr);
+}
+
+void readelf_L(Elf32_Phdr *phdr, int j)
+{
+    char *type;
+    char flag[3];
+    if (j == 0)
+    {
+        printf("TYPE    Offset   VirtAdrr   PhysAdrr   Filesiz   Memsiz  FLG Align\n");
+    }
+    switch (phdr->p_type)
+    {
+    case PT_NULL:
+        type = "NULL";
+        break;
+    case PT_LOAD:
+        type = "LOAD";
+        break;
+    case PT_DYNAMIC:
+        type = "DYNAMIC";
+        break;
+    case PT_INTERP:
+        type = "INTERP";
+        break;
+    case PT_NOTE:
+        type = "NOTE";
+        break;
+    case PT_SHLIB:
+        type = "SHLIB";
+        break;
+    case PT_PHDR:
+        type = "PHDR";
+        break;
+
+    default:
+        type = "ELSE";
+        break;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        flag[i] = ' ';
+    }
+    if (phdr->p_flags >= 0b100)
+    {
+        flag[0] = 'R';
+    }
+    if (phdr->p_flags >= 0b10 && (phdr->p_flags < 0b100 || phdr->p_flags - 0b100 >= 0b10))
+    {
+        flag[1] = 'W';
+    }
+    if (phdr->p_flags % 2 == 1)
+    {
+        flag[2] = 'E';
+    }
+
+    printf(format, type, phdr->p_offset, phdr->p_vaddr,  phdr->p_paddr, phdr->p_filesz, phdr->p_memsz, flag[0], flag[1], flag[2], phdr->p_align);
+}
+
+int foreach_phdr(void *map_start, void (*func)(Elf32_Phdr *, int), int arg)
+{
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)map_start;
+    if (checkMagicNumber(ehdr->e_ident))
+    {
+
+        Elf32_Phdr *phdr_table = (Elf32_Phdr *)((char *)map_start + ehdr->e_phoff);
+
+        for (int i = 0; i < ehdr->e_phnum; i++)
+        {
+            func(&phdr_table[i], i);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
     int op = -1;
-    Elf32_Ehdr *header = malloc(52);
-    if (argc < 2)
+
+    Elf32_Ehdr *header = NULL;
+
+    if (argc < 2) // No file name given
     {
         printf("Usage : ./my_loader <file_name>\n");
         exit(0);
     }
     op = open(argv[1], O_RDWR);
-    if (op < 0)
+    if (op < 0) // cannot open file
     {
         fprintf(stderr, "ERROR opening file %s", argv[1]);
         perror(" ");
         exit(0);
     }
 
+    lseek(op, 0, SEEK_SET);
+    int size = lseek(op, 0, SEEK_END);
+    lseek(op, 0, SEEK_SET);
+    header = malloc(size);
     read(op, header, sizeof(*header));
-    if (checkMagicNumber(header->e_ident) == 0)
+    if (checkMagicNumber(header->e_ident) == 0) // file is not ELF32
     {
         fprintf(stderr, "ERROR reading file %s : Not an ELF32 File\n", argv[1]);
         close(op);
@@ -37,7 +123,9 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    printf("%s\n", header->e_ident);
+    void *map = mmap(NULL, size, PROT_READ, MAP_PRIVATE, op, 0);
+
+    foreach_phdr(map, readelf_L, 0);
 
     close(op);
     free(header);
